beb07d9f7459798f83988b74d109dc62eb8b1b78
antoniocostantin <costantini062@gmail.com>
2024-09-27 16:23:41 +0200

    Delete Progsett1BackEnd directory

--- Progsett1BackEnd/src/ElementoMultimediale/ElementoMultimediale.java
+++
@@ -1,8 +0,0 @@
-package ElementoMultimediale;
-
-import ElementoMultimediale.elementi.Playable;
-
-public abstract class ElementoMultimediale {
-    private String titolo;
-    public abstract void play();
-}
--- Progsett1BackEnd/src/ElementoMultimediale/elementi/Showable.java
+++
@@ -1,9 +0,0 @@
-package ElementoMultimediale.elementi;
-
-public interface Showable {
-    public void setLuminosità(char lum);
-    public void setLuminosità(int lum);
-    public void aumentaLum();
-    public void diminuisciLum();
-    public int getLum();
-}
