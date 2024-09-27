3cc6506532a4776d3e1e978897cc4241a95e9140
antoniocostantin <costantini062@gmail.com>
2024-09-27 19:33:09 +0200

    Delete Progsett1BackEnd directory

--- Progsett1BackEnd/src/ElementoMultimediale/elementi/Showable.java
+++
@@ -1,8 +0,0 @@
-package ElementoMultimediale.elementi;
-
-public interface Showable {
-    public void setLuminosità(int lum);
-    public void aumentaLum();
-    public void diminuisciLum();
-    public int getLum();
-}
